f1a2119d94ed72c7802fe701245409c98aceed6a
Terho Uotila <ttuotila@cc.helsinki.fi>
2016-03-12 21:44:18 +0200

    postgres lisää

--- TikapeVanhainkoti/src/main/java/sovelluslogiikka/KetjuDAO.java
+++ TikapeVanhainkoti/src/main/java/sovelluslogiikka/KetjuDAO.java
@@ -116,7 +116,7 @@ public class KetjuDAO implements Dao<Integer, Ketju> {
                 + "LEFT JOIN Alue "
                 + "ON Alue.Id = Ketju.AlueId "
                 + "WHERE Ketju.Id = ? "
-                + "GROUP BY Ketju.Id "
+                + "GROUP BY Ketju.Id, an "
                 + "ORDER BY MAX(Viesti.pvm) DESC;");
 
         stmt.setInt(1, kid);
